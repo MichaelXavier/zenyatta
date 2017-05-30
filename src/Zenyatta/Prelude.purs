10eca103c4cf7549b6f5f120be03eba2b3330d64
Michael Xavier <michael@michaelxavier.net>
2017-05-29 21:12:33 -0700

    Port view to pux

--- src/Zenyatta/Prelude.purs
+++ src/Zenyatta/Prelude.purs
@@ -10,6 +10,7 @@ module Zenyatta.Prelude
   , module Data.Foldable
   , module Data.Traversable
   , noop
+  , classes
   ) where
 
 
@@ -18,14 +19,22 @@ import Control.Monad.Eff
 import Control.Monad.Eff.Class
 import Control.Monad.Eff.Console
 import Data.NonEmpty (NonEmpty(..), (:|))
+import Data.String as String
 import Data.Maybe
 import Data.Monoid
 import Data.Either
 import Data.Foldable
 import Data.Traversable
+import Text.Smolder.Markup as TSM
+import Text.Smolder.HTML.Attributes as TSHA
 import Prelude
 -------------------------------------------------------------------------------
 
 
 noop :: forall f. Applicative f => f Unit
 noop = pure unit
+
+
+-------------------------------------------------------------------------------
+classes :: Array String -> TSM.Attribute
+classes = TSHA.className <<< String.joinWith " "
--- src/Zenyatta/Timer.purs
+++ src/Zenyatta/Timer.purs
@@ -6,10 +6,84 @@ module Zenyatta.Timer
 -------------------------------------------------------------------------------
 import Pux.DOM.HTML as PH
 import Zenyatta.Types as T
-import Text.Smolder.Markup (text)
+import Text.Smolder.HTML (div, h1, nav)
+import Text.Smolder.HTML.Attributes (style)
+import Text.Smolder.Markup (Markup, parent, text, (!))
+import Zenyatta.Prelude hiding (div)
 -------------------------------------------------------------------------------
 
 
 --Probably take a timer state?
+--TODO: break up
 view :: T.State -> PH.HTML T.Event
-view _  = text "timer"
+view _  = main ! classes ["main"] $ do
+  header
+  content
+  where
+    header = do
+      nav ! classes ["header"] $ do
+        --TODO: active on state
+        div ! classes ["tab-button", "active"] $ do
+          text "Timer"
+        div ! classes ["tab-button"] $ do
+          text "Logs"
+
+    content = do
+      div ! classes ["content"] $ do
+        mainTimer
+        chimeTimer
+        timerControls
+
+    mainTimer = do
+      div ! classes ["main-timer", "timer-block"] $ do
+        div ! classes ["gutter"] $ mempty
+        div ! classes ["timer"] $ do
+          h1 ! classes ["timer-title"] $ do
+            text "Timer"
+          div ! classes ["center-block"] $ do
+            div ! classes ["minutes"] $ do
+              div ! classes ["minutes-control", "control", "plus"] $ mempty
+              div ! classes ["minutes-display"] $ do
+                text "3"
+              div ! classes ["minutes-control", "control", "minus"] $ mempty
+            div ! classes ["seconds"] $ do
+              div ! classes ["seconds-control", "control"] $ mempty
+              div ! classes ["seconds-display"] $ do
+                text ":59"
+              div ! classes ["seconds-control", "control"] $ mempty
+          div ! classes ["progress-bar"] $ do
+            --TODO: could we use purescript-css on the attribute?
+            div ! classes ["progress-bar-fill"] ! style "width: 75%;" $ mempty
+        div ! classes ["gutter"] $ mempty
+
+    chimeTimer = do
+      div ! classes ["chime-timer", "timer-block"] $ do
+        div ! classes ["gutter"] $ mempty
+        div ! classes ["timer"] $ do
+          h1 ! classes ["timer-title"] $ do
+            text "Chime"
+          div ! classes ["center-block"] $ do
+            div ! classes ["minutes"] $ do
+              div ! classes ["minutes-control", "control", "plus"] $ mempty
+              div ! classes ["minutes-display"] $ do
+                text "3"
+              div ! classes ["minutes-control", "control", "minus"] $ mempty
+            div ! classes ["seconds"] $ do
+              div ! classes ["seconds-control", "control"] $ mempty
+              div ! classes ["seconds-display"] $ do
+                text ":59"
+              div ! classes ["seconds-control", "control"] $ mempty
+          div ! classes ["progress-bar"] $ do
+            --TODO: could we use purescript-css on the attribute?
+            div ! classes ["progress-bar-fill"] ! style "width: 75%;" $ do
+              mempty
+        div ! classes ["gutter"] $ mempty
+
+    timerControls = do
+      div ! classes ["controls"] $ do
+        div ! classes ["undo", "control"] $ mempty
+        div ! classes ["play", "control"] $ mempty
+
+-------------------------------------------------------------------------------
+main :: forall e. Markup e -> Markup e
+main = parent "main"
